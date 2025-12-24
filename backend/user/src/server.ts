b45c91acaf34f0b5a426a9cef72c8946ed56261b
Sah-Rahul <sahfamilyinfo@gmail.com>
2025-12-24 12:16:30 +0545

    setup redis with upstash

--- backend/user/src/server.ts
+++ backend/user/src/server.ts
@@ -1,12 +1,23 @@
 import dotenv from "dotenv";
 import express from "express";
 import { connectDB } from "./config/db";
-
+import { createClient } from "redis";
 dotenv.config();
 
 const app = express();
 
 connectDB()
+export const redisClient = createClient({
+  url:process.env.REDIS_URL
+})
+
+redisClient
+.connect()
+.then(()=> console.log("connected to the redis"))
+.catch((error
+)=> console.log("connection failed to the redis",error.message))
+
+
 const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
